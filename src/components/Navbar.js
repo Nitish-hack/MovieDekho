7b51792c15ef1873c59f2da1e31ae6bd0ee8013b
Nitish Kumar Jha <nitishjha913@gmail.com>
2022-11-17 22:43:44 +0530

    responsiveness added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg  navbar-box">
         <div className="d-flex container-fluid  py-1 px-3 ">
 
-
+        <Link className="navbar-brand nav-logo  " to="/">MovieDekho</Link>
 
           <ul className="navbar-nav menu px-4  nav-scroll">
 
@@ -36,16 +36,7 @@ const Navbar = () => {
               <p className='text-white navDisplaynone m-0 text-center'>Search</p>
             </li>
             
-            <li className="nav-item dropdown dropdown-hover mx-4">
-        <button className="btn btn-secondary dropdown-toggle fs-4 text-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">Rated </button>
-        <ul className="dropdown-menu">
-        <li><Link to="/toprated/all/all" className="dropdown-item ">All</Link></li>
-                <li><Link to="/toprated/IN/hi" className="dropdown-item">India</Link></li>
-                <li><Link to="/toprated/KR/ko" className="dropdown-item">korea</Link></li>
-                <li><Link to="/toprated/JP/ja" className="dropdown-item">Japan</Link></li>
-                <li><Link to="/toprated/US/en" className="dropdown-item">US</Link></li>
-        </ul>
-      </li>
+           
 
 
 
@@ -54,9 +45,21 @@ const Navbar = () => {
       
 
           </ul>
+          
+          <div className="nav-item dropdown dropdown-hover d-menu">
+        <button className="btn btn-secondary dropdown-toggle fs-4 text-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">Rated </button>
+        <ul className="dropdown-menu ">
+        
+              <li ><Link className="dropdown-item" to="/toprated/all/all">All</Link></li>
+              <li ><Link className="dropdown-item" to="/toprated/IN/hi" >India</Link></li>
+              <li ><Link className="dropdown-item" to="/toprated/KR/ko" >korea</Link></li>
+              <li ><Link className="dropdown-item" to="/toprated/JP/ja">Japan</Link></li>
+              <li ><Link className="dropdown-item" to="/toprated/US/en">US</Link></li>
+      
+        </ul>
+      </div>
 
-
-          <Link className="navbar-brand nav-logo  " to="/">MovieDekho</Link>
+         
 
 
         </div>
@@ -65,15 +68,15 @@ const Navbar = () => {
       <ul className=" d-flex nav-scroll py-3 ">
 
 
-        <li className="nav-item fs-5 mx-5"><Link to="/mystery" className="nav-link active text-white">Mystery</Link></li>
-        <li className="nav-item fs-5 mx-5"><Link to="/comedy" className="nav-link active text-white">Comedy</Link></li>
-        <li className="nav-item fs-5 mx-5"><Link to="/horror" className="nav-link active text-white">Horror</Link></li>
-        <li className="nav-item fs-5 mx-5"><Link to="/action" className="nav-link active text-white">Action</Link></li>
-        <li className="nav-item fs-5 mx-5"><Link to="/scifi" className="nav-link active text-white text-nowrap">Sci-Fi</Link></li>
-        <li className="nav-item fs-5 mx-5"><Link to="/romance" className="nav-link active text-white">Romance</Link></li>
-        <li className="nav-item fs-5 mx-5"><Link to="/animation" className="nav-link active text-white">Animation</Link></li>
-        <li className="nav-item fs-5 mx-5"><Link to="/adventure" className="nav-link active text-white">Adventure</Link></li>
-        <li className="nav-item fs-5 mx-5"><Link to="/adventure" className="nav-link active text-white">Drama</Link></li>
+        <li className="nav-item fs-5 mx-4 genre"><Link to="/mystery" className="nav-link active text-white">Mystery</Link></li>
+        <li className="nav-item fs-5 mx-4 genre"><Link to="/comedy" className="nav-link active text-white">Comedy</Link></li>
+        <li className="nav-item fs-5 mx-4 genre"><Link to="/horror" className="nav-link active text-white">Horror</Link></li>
+        <li className="nav-item fs-5 mx-4 genre"><Link to="/action" className="nav-link active text-white">Action</Link></li>
+        <li className="nav-item fs-5 mx-4 genre"><Link to="/scifi" className="nav-link active text-white text-nowrap">Sci-Fi</Link></li>
+        <li className="nav-item fs-5 mx-4 genre"><Link to="/romance" className="nav-link active text-white">Romance</Link></li>
+        <li className="nav-item fs-5 mx-4 genre"><Link to="/animation" className="nav-link active text-white">Animation</Link></li>
+        <li className="nav-item fs-5 mx-4 genre"><Link to="/adventure" className="nav-link active text-white">Adventure</Link></li>
+        <li className="nav-item fs-5 mx-4 genre"><Link to="/adventure" className="nav-link active text-white">Drama</Link></li>
 
       </ul>
     </div>
